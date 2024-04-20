49f0f031fe50e0c10c379129160af7bb965f3098
ellayang <ellayang@mail.apex.com.tw>
2024-04-20 20:11:56 +0800

    新增及編輯貼文的 content 欄位新增 trim()、新增編輯貼文傳入多餘欄位判斷、新增刪除單一貼文的 try catch

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,7 @@ const posts = {
     async createPosts (req, res, next) {
         try {
             const { user, image, content, type, tags } = req.body;
-            const addPost = await Post.create({ user, image, content, type, tags });
+            const addPost = await Post.create({ user, image, content: content.trim(), type, tags });
             successHandle(res, addPost);
         } catch ({ errors }) {
             errorHandle(res, 400, 'format', errors);
@@ -34,23 +34,39 @@ const posts = {
         successHandle(res, posts);
     },
     async deletePost (req, res, next) {
-        const { id } = req.params;
-        const delPost = await Post.findByIdAndDelete(id);
-        if (delPost) {
-            successHandle(res, delPost)
-        } else {
-            errorHandle(res, 400, 'id');
+        try {
+            const { id } = req.params;
+            const delPost = await Post.findByIdAndDelete(id);
+            if (delPost) {
+                successHandle(res, delPost)
+            } else {
+                errorHandle(res, 400, 'id');
+            }
+        } catch ({ errors }) {
+            errorHandle(res, 400, 'id', errors);
         }
-        
     },
     async editPost (req, res, next) {
         try {
-            if (!Object.keys(req.body).length) {
+            const { body, params } = req;
+
+            if (!Object.keys(body).length) {
                 throw new Error();
             } else {
-                const { image, content, type, tags } = req.body;
-                const updateData = { image, content, type, tags };
-                const { id } = req.params;
+                // 檢查是否有多餘欄位
+                const fields = ['name', 'image', 'content', 'type', 'tags'];
+                const excessFields = Object.keys(body).reduce((accumulator, currentValue) => {
+                    if (!fields.includes(currentValue)) { accumulator.push(currentValue) }
+                    return accumulator;
+                }, []);
+
+                if (excessFields.length) {
+                   return errorHandle(res, 400, 'format', `不應包含 ${ excessFields.join('、') } 欄位`);
+                }
+                
+                const { image, content, type, tags } = body;
+                const updateData = { image, content: content?.trim(), type, tags };
+                const { id } = params;
                 // new 參數指定是否返回更新後的文件
                 // runValidators 參數指定是否在更新時 進行 Schema 定義的驗證器
                 const updatePost = await Post.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
